dcf879f5cbc3e760d7655e6becbb8ec7b09c34b0
Ivan Krstic <krstic8687@yahoo.com>
2025-03-18 15:34:16 +0100

    170 - create useMovies custom hook

---
+++ usepopcorn/src/useMovies.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+const KEY = "e602e10b";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const response = await fetch(
+            `http://omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!response.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await response.json();
+          if (data.Response === "False") throw new Error("Movie not fount");
+
+          setMovies(() => data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
